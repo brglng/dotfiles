decac60e1c8d16c06327a0764fae488d92b9bc74
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2019-06-15 23:57:38 +0800

    Make link.sh does not overwrite bashrc zshrc vimrc and init.vim

--- link.sh
+++ link.sh
@@ -1,48 +1,21 @@
 #!/usr/bin/env bash
 
-link() {
-  mkdir -p ~/.config/alacritty
-  [ -e ~/.config/alacritty/alacritty.yml ] && [ ! -L ~/.config/alacritty/alacritty.yml ] && mv -f ~/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml.orig
-  ln -fs $(pwd)/config/alacritty/alacritty.yml ~/.config/alacritty/
+update_bashrc() {
+  mkdir -p /tmp/brglng/dotfiles
+  cat << EOF > /tmp/brglng/dotfiles/bashrc
+# BEGIN brglng/dotfiles
+[ -r $(pwd)/bashrc ] && . $(pwd)/bashrc
+# END brglng/dotfiles
+EOF
 
-  [ -e ~/.bashrc ] && [ ! -L ~/.bashrc ] && mv -f ~/.bashrc ~/.bashrc.orig
-  ln -fs $(pwd)/bashrc ~/.bashrc
-
-  mkdir -p ~/.cgdb
-  [ -e ~/.cgdb/cgdbrc ] && [ ! -L ~/.cgdb/cgdbrc ] && mv -f ~/.cgdb/cgdbrc ~/.cgdb/cgdbrc.orig
-  ln -fs $(pwd)/cgdb/cgdbrc ~/.cgdb/
-
-  [ -e ~/.gitignore_global ] && [ ! -L ~/.gitignore_global ] && mv -f ~/.gitignore_global ~/.gitignore_global.orig
-  ln -fs $(pwd)/gitignore_global ~/.gitignore_global
-
-  [ -e ~/.tmux.conf ] && [ ! -L ~/.tmux.conf ] && mv -f ~/.tmux.conf ~/.tmux.conf.orig
-  ln -fs $(pwd)/tmux.conf ~/.tmux.conf
-
-  mkdir -p ~/.config
-  if [ -e ~/.config/nvim ]; then
-    if [ -L ~/.config/nvim ]; then
-      rm -f ~/.config/nvim
-    else
-      mv -f ~/.config/nvim ~/.config/nvim.orig
-    fi
+  if [ ! -e $1 ] || [ "$(perl -n0e 'print $1 if /(# BEGIN brglng\/dotfiles.*# END brglng\/dotfiles)/s' $1)" = "" ]; then
+    cat /tmp/brglng/dotfiles/bashrc >> $1
+  else
+    perl -i -p0e 's/# BEGIN brglng\/dotfiles.*# END brglng\/dotfiles/`cat \/tmp\/brglng\/dotfiles\/bashrc`/gse' $1
   fi
-  ln -s $(pwd)/vim ~/.config/nvim
+}
 
-  if [ -e ~/.vim ]; then
-    if [ -L ~/.vim ]; then
-      rm -f ~/.vim
-    else
-      mv -f ~/.vim ~/.vim.orig
-    fi
-  fi
-  ln -s $(pwd)/vim ~/.vim
-
-  [ -e ~/.vimrc ] && [ ! -L ~/.vimrc ] && mv -f ~/.vimrc ~/.vimrc.orig
-  ln -fs $(pwd)/vimrc ~/.vimrc
-
-  [ -e ~/.zshrc ] && [ ! -L ~/.zshrc ] && mv -f ~/.zshrc ~/.zshrc.orig
-  ln -fs $(pwd)/zshrc ~/.zshrc
-
+update_gitconfig() {
   mkdir -p /tmp/brglng/dotfiles
   cat << EOF > /tmp/brglng/dotfiles/gitconfig
 # BEGIN brglng/dotfiles
@@ -56,6 +29,61 @@ EOF
   else
     perl -i -p0e 's/# BEGIN brglng\/dotfiles.*# END brglng\/dotfiles/`cat \/tmp\/brglng\/dotfiles\/gitconfig`/gse' $HOME/.gitconfig
   fi
+}
+
+update_vimrc() {
+  mkdir -p /tmp/brglng/dotfiles
+  cat << EOF > /tmp/brglng/dotfiles/vimrc
+" BEGIN brglng/dotfiles
+let &runtimepath = '$(pwd)/vim,' . &runtimepath . ',$(pwd)/vim/after'
+source /$(pwd)/dotfiles/vim/init.vim
+" END brglng/dotfiles
+EOF
+
+  if [ ! -e $1 ] || [ "$(perl -n0e 'print $1 if /(" BEGIN brglng\/dotfiles.*" END brglng\/dotfiles)/s' $1)" = "" ]; then
+    cat /tmp/brglng/dotfiles/vimrc >> $1
+  else
+    perl -i -p0e 's/" BEGIN brglng\/dotfiles.*" END brglng\/dotfiles/`cat \/tmp\/brglng\/dotfiles\/vimrc`/gse' $1
+  fi
+}
+
+update_zshrc() {
+  mkdir -p /tmp/brglng/dotfiles
+  cat << EOF > /tmp/brglng/dotfiles/zshrc
+# BEGIN brglng/dotfiles
+[ -r $(pwd)/zshrc ] && . $(pwd)/zshrc
+# END brglng/dotfiles
+EOF
+
+  if [ ! -e $HOME/.zshrc ] || [ "$(perl -n0e 'print $1 if /(# BEGIN brglng\/dotfiles.*# END brglng\/dotfiles)/s' $1)" = "" ]; then
+    cat /tmp/brglng/dotfiles/zshrc >> $HOME/.zshrc
+  else
+    perl -i -p0e 's/# BEGIN brglng\/dotfiles.*# END brglng\/dotfiles/`cat \/tmp\/brglng\/dotfiles\/zshrc`/gse' $1
+  fi
+}
+
+link() {
+  mkdir -p ~/.config/alacritty
+  [ -e ~/.config/alacritty/alacritty.yml ] && [ ! -L ~/.config/alacritty/alacritty.yml ] && mv -f ~/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml.orig
+  ln -fs $(pwd)/config/alacritty/alacritty.yml ~/.config/alacritty/
+
+  update_bashrc $HOME/.bashrc
+
+  mkdir -p ~/.cgdb
+  [ -e ~/.cgdb/cgdbrc ] && [ ! -L ~/.cgdb/cgdbrc ] && mv -f ~/.cgdb/cgdbrc ~/.cgdb/cgdbrc.orig
+  ln -fs $(pwd)/cgdb/cgdbrc ~/.cgdb/
+
+  [ -e ~/.gitignore_global ] && [ ! -L ~/.gitignore_global ] && mv -f ~/.gitignore_global ~/.gitignore_global.orig
+  ln -fs $(pwd)/gitignore_global ~/.gitignore_global
+
+  [ -e ~/.tmux.conf ] && [ ! -L ~/.tmux.conf ] && mv -f ~/.tmux.conf ~/.tmux.conf.orig
+  ln -fs $(pwd)/tmux.conf ~/.tmux.conf
+
+  mkdir -p ~/.config/nvim
+  update_vimrc $HOME/.config/nvim/init.vim
+  update_vimrc $HOME/.vimrc
+  update_gitconfig
+  update_zshrc
 
   mkdir -p ~/.local/bin
   ln -fs $(pwd)/local/bin/* ~/.local/bin/
@@ -67,24 +95,10 @@ link_linux() {
 
 link_mac() {
   link
-
-  mkdir -p /tmp/brglng/dotfiles
-  cat << EOF > /tmp/brglng/dotfiles/bash_profile
-# BEGIN brglng/dotfiles
-[ -r ~/.bashrc ] && . ~/.bashrc
-# END brglng/dotfiles
-EOF
-
-  if [ ! -e $HOME/.bash_profile ] || [ "$(perl -n0e 'print $1 if /(# BEGIN brglng\/dotfiles.*# END brglng\/dotfiles)/s' $HOME/.bash_profile)" = "" ]; then
-    cat /tmp/brglng/dotfiles/bash_profile >> ~/.bash_profile
-  else
-    perl -i -p0e 's/# BEGIN brglng\/dotfiles.*# END brglng\/dotfiles/`cat \/tmp\/brglng\/dotfiles\/bash_profile`/gse' $HOME/.bash_profile
-  fi
+  update_bashrc $HOME/.bash_profile
 }
 
 case $(uname -s) in
   Linux) link_linux ;;
   Darwin) link_mac ;;
 esac
-
-rm -rf ~/.cache/dein/state*.vim
