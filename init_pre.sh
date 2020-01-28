ef8d944f61498a173b561fd2109bd694782bb34f
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2020-01-28 23:12:03 +0800

    Add python binary path

--- init_pre.sh
+++ init_pre.sh
@@ -10,6 +10,13 @@ path_prepend() {
     fi
 }
 
+if [ "`uname -s`" = Darwin ];
+    [ -d ~/Library/Python/3.5/bin ] && path_prepend ~/Library/Python/3.5/bin
+    [ -d ~/Library/Python/3.6/bin ] && path_prepend ~/Library/Python/3.6/bin
+    [ -d ~/Library/Python/3.7/bin ] && path_prepend ~/Library/Python/3.7/bin
+    [ -d ~/Library/Python/3.8/bin ] && path_prepend ~/Library/Python/3.8/bin
+fi
+
 path_prepend "$HOME/.local/bin"
 path_prepend "$HOME/.cargo/bin"
 
