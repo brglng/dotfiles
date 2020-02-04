3397a490ecb5a9c742c16742f3b3c2e41cd76261
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2020-02-04 22:32:50 +0800

    Fix

--- shell_rc_pre.sh
+++ shell_rc_pre.sh
@@ -30,11 +30,12 @@ fi
 if [[ "$NVM_DIR" = "" && $HOMEBREW_PREFIX != "" ]]; then
     export NVM_DIR="$HOME/.nvm"
     [[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ]] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"  # This loads nvm
+    [[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ]] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 fi
 
 if [[ "$GOPATH" = "" ]]; then
     export GOPATH="$HOME/go"
-    path_append 
+    path_append "$GOPATH/bin"
 fi
 
 export HOMEBREW_NO_AUTO_UPDATE=1
