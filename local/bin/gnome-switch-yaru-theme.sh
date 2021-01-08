e6f9f39dbaf31ed234ebcb332986fa675b01b5ce
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2021-01-08 12:14:27 +0800

    rename

--- local/bin/gnome-switch-yaru-theme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PID=$(pgrep -u $LOGNAME gnome-session | head -1)
-export $(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ)
-gsettings set org.gnome.desktop.interface gtk-theme "Yaru-$1"
