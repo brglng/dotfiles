042ba4708adab29cf6fc9eab13b98b644f3b83f4
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2024-11-14 15:52:27 +0800

    fix(nvim): indentation

--- config/nvim/lua/plugins/ui/alpha.lua
+++ config/nvim/lua/plugins/ui/alpha.lua
@@ -18,7 +18,7 @@ return {
         local function utf8_len(s, pos)
             local byte = string.byte(s, pos)
             if not byte then
-          	return nil
+            return nil
             end
             return (byte < 0x80 and 1) or (byte < 0xE0 and 2) or (byte < 0xF0 and 3) or (byte < 0xF8 and 4) or 1
         end
@@ -33,27 +33,27 @@ return {
             }
 
             for key, color in pairs(colors) do
-          	local name = "Alpha" .. key
-          	vim.api.nvim_set_hl(0, name, color)
-          	colors[key] = name
+                local name = "Alpha" .. key
+                vim.api.nvim_set_hl(0, name, color)
+                colors[key] = name
             end
 
             result.opts.hl = {}
             for i, line in ipairs(logoColors) do
-          	local highlights = {}
-          	local pos = 0
+                local highlights = {}
+                local pos = 0
 
-          	for j = 1, #line do
-          	    local opos = pos
-          	    pos = pos + utf8_len(logo[i], opos + 1)
+                for j = 1, #line do
+                    local opos = pos
+                    pos = pos + utf8_len(logo[i], opos + 1)
 
-          	    local color_name = colors[line:sub(j, j)]
-          	    if color_name then
-          		table.insert(highlights, { color_name, opos, pos })
-          	    end
-          	end
+                    local color_name = colors[line:sub(j, j)]
+                    if color_name then
+                        table.insert(highlights, { color_name, opos, pos })
+                    end
+                end
 
-          	table.insert(result.opts.hl, highlights)
+                table.insert(result.opts.hl, highlights)
             end
             return { result }
         end
@@ -314,47 +314,47 @@ return {
                     type = "group",
                     val = function()
                         return apply_colors({
-  	                    [[  ███       ███  ]],
-  	                    [[  ████      ████ ]],
-  	                    [[  ████     █████ ]],
-  	                    [[ █ ████    █████ ]],
-  	                    [[ ██ ████   █████ ]],
-  	                    [[ ███ ████  █████ ]],
-  	                    [[ ████ ████ ████ ]],
-  	                    [[ █████  ████████ ]],
-  	                    [[ █████   ███████ ]],
-  	                    [[ █████    ██████ ]],
-  	                    [[ █████     █████ ]],
-  	                    [[ ████      ████ ]],
-  	                    [[  ███       ███  ]],
-  	                    -- [[                    ]],
-  	                    -- [[  N  E  O  V  I  M  ]],
-  	                }, {
-  	                    ["b"] = { fg = "#3399ff", ctermfg = 33 },
-  	                    ["a"] = { fg = "#53C670", ctermfg = 35 },
-  	                    ["g"] = { fg = "#39ac56", ctermfg = 29 },
-  	                    ["h"] = { fg = "#33994d", ctermfg = 23},
-  	                    ["i"] = { fg = "#33994d", bg = "#39ac56", ctermfg = 23, ctermbg = 29},
-  	                    ["j"] = { fg = "#53C670", bg = "#33994d", ctermfg = 35, ctermbg = 23 },
-  	                    ["k"] = { fg = "#30A572", ctermfg = 36},
-  	                }, {
-  	                    [[  kkkka       gggg  ]],
-  	                    [[  kkkkaa      ggggg ]],
-  	                    [[ b kkkaaa     ggggg ]],
-  	                    [[ bb kkaaaa    ggggg ]],
-  	                    [[ bbb kaaaaa   ggggg ]],
-  	                    [[ bbbb aaaaaa  ggggg ]],
-  	                    [[ bbbbb aaaaaa igggg ]],
-  	                    [[ bbbbb  aaaaaahiggg ]],
-  	                    [[ bbbbb   aaaaajhigg ]],
-  	                    [[ bbbbb    aaaaajhig ]],
-  	                    [[ bbbbb     aaaaajhi ]],
-  	                    [[ bbbbb      aaaaajh ]],
-  	                    [[  bbbb       aaaaa  ]],
-  	                    -- [[                    ]],
-  	                    -- [[  a  a  a  b  b  b  ]],
-  	                })
-  	            end
+                            [[  ███       ███  ]],
+                            [[  ████      ████ ]],
+                            [[  ████     █████ ]],
+                            [[ █ ████    █████ ]],
+                            [[ ██ ████   █████ ]],
+                            [[ ███ ████  █████ ]],
+                            [[ ████ ████ ████ ]],
+                            [[ █████  ████████ ]],
+                            [[ █████   ███████ ]],
+                            [[ █████    ██████ ]],
+                            [[ █████     █████ ]],
+                            [[ ████      ████ ]],
+                            [[  ███       ███  ]],
+                            -- [[                    ]],
+                            -- [[  N  E  O  V  I  M  ]],
+                        }, {
+                            ["b"] = { fg = "#3399ff", ctermfg = 33 },
+                            ["a"] = { fg = "#53C670", ctermfg = 35 },
+                            ["g"] = { fg = "#39ac56", ctermfg = 29 },
+                            ["h"] = { fg = "#33994d", ctermfg = 23},
+                            ["i"] = { fg = "#33994d", bg = "#39ac56", ctermfg = 23, ctermbg = 29},
+                            ["j"] = { fg = "#53C670", bg = "#33994d", ctermfg = 35, ctermbg = 23 },
+                            ["k"] = { fg = "#30A572", ctermfg = 36},
+                        }, {
+                            [[  kkkka       gggg  ]],
+                            [[  kkkkaa      ggggg ]],
+                            [[ b kkkaaa     ggggg ]],
+                            [[ bb kkaaaa    ggggg ]],
+                            [[ bbb kaaaaa   ggggg ]],
+                            [[ bbbb aaaaaa  ggggg ]],
+                            [[ bbbbb aaaaaa igggg ]],
+                            [[ bbbbb  aaaaaahiggg ]],
+                            [[ bbbbb   aaaaajhigg ]],
+                            [[ bbbbb    aaaaajhig ]],
+                            [[ bbbbb     aaaaajhi ]],
+                            [[ bbbbb      aaaaajh ]],
+                            [[  bbbb       aaaaa  ]],
+                            -- [[                    ]],
+                            -- [[  a  a  a  b  b  b  ]],
+                        })
+                    end
                 },
                 {
                     type = "text",
