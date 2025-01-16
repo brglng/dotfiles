72d6fe3d28c3d1d7ee1fa84052250994e2940f30
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2025-01-16 14:52:22 +0800

    update

--- config/nvim/lua/plugins/session/neovim_project.lua
+++ config/nvim/lua/plugins/session/neovim_project.lua
@@ -3,41 +3,42 @@ return {
     lazy = false,
     priority = 100,
     event = "VeryLazy",
-    opts = {
-        projects = { -- define project roots
-            "~/Syncthing/vps/dotfiles",
-            "~/Syncthing/sync/work/loostone/fdn_reverb",
-            "~/Syncthing/sync/work/loostone/pm-audio-lib/*",
-            "~/Syncthing/notes",
-            "~/Syncthing/projects/*",
-            "~/Syncthing/projects/vim-plugins/*",
-            "~/Syncthing/bose/github/*",
-            "/mnt/c/bose/github/*",
-            "C:/bose/github/*",
-            "C:/bose/svn/asd2/volvo_gpa/branches/*",
-            "C:/bose/svn/asd2/volvo_gpa/branches/*/audio/simulink",
-            "C:/bose/svn/asd2/volvo_gpa/branches/*/audio/simulink/application/model/code",
-            "C:/bose/svn/asd2/volvo_gpa/tags/*",
-            "C:/bose/svn/asd2/volvo_gpa/tags/*/audio/simulink",
-            "C:/bose/svn/asd2/volvo_gpa/tags/*/audio/simulink/application/model/code",
-            "C:/bose/svn/asd2/volvo_gpa/trunk",
-            "C:/bose/svn/asd2/volvo_gpa/trunk/audio/simulink",
-            "C:/bose/svn/asd2/volvo_gpa/trunk/audio/simulink/application/model/code",
-            "C:/bose/AWE/mag7Control_module",
-            "C:/DSP Concepts/AWE Designer * Pro/CustomModules/*"
-        },
-        dashboard_mode = false,
-        last_session_on_startup = false
-    },
-    init = function()
-        -- enable saving the state of plugins in the session
-        -- vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
-    end,
     dependencies = {
         { "nvim-lua/plenary.nvim" },
         -- { "nvim-telescope/telescope.nvim" },
         { "Shatur/neovim-session-manager" },
     },
+    init = function()
+        -- enable saving the state of plugins in the session
+        -- vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+    end,
+    opts = {
+        projects = { -- define project roots
+            "~/Syncthing/vps/dotfiles",
+            "~/Syncthing/notes",
+            "~/Syncthing/projects/*",
+            "~/Syncthing/projects/vim-plugins/*",
+        },
+        dashboard_mode = false,
+        last_session_on_startup = false
+    },
+    config = function(_, opts)
+        local private_projects = vim.fn.expand("~/Syncthing/vps/dotfiles/config/nvim/lua/brglng/private_projects.lua")
+        local local_projects = vim.fn.stdpath("data") .. "/neovim-project/projects.lua"
+        if vim.uv.fs_open(private_projects, "r", 438) then
+            vim.list_extend(opts.projects, dofile(private_projects))
+        end
+        if vim.uv.fs_open(local_projects, "r", 438) then
+            vim.list_extend(opts.projects, dofile(local_projects))
+        end
+        vim.api.nvim_create_user_command("EditPrivateProjects", function()
+            vim.cmd("edit " .. private_projects)
+        end, { nargs = 0 })
+        vim.api.nvim_create_user_command("EditLocalProjects", function()
+            vim.cmd("edit " .. local_projects)
+        end, { nargs = 0 })
+        require("neovim-project").setup(opts)
+    end,
     keys = {
         { "<Leader>fp", mode = "n", "<Cmd>NeovimProjectHistory<CR>", desc = "Recent Projects" },
         { "<Leader>fP", mode = "n", "<Cmd>NeovimProjectDiscover<CR>", desc = "All Projects" }
