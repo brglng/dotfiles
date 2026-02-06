eb2e0b28eeaf65a48d643c58272ceba7e720e613
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2026-02-06 15:34:42 +0800

    feat(nvim): window_picker keys

--- config/nvim/lua/plugins/ui/window_picker.lua
+++ config/nvim/lua/plugins/ui/window_picker.lua
@@ -1,7 +1,6 @@
 return {
     "s1n7ax/nvim-window-picker",
     event = 'VeryLazy',
-    lazy = true,
     version = '2.*',
     opts = {
         hint = "floating-big-letter",
@@ -11,15 +10,29 @@ return {
             include_current_win = false,
             bo = {
                 filetype = {
-                    'neo-tree',
+                    -- 'neo-tree',
                     "neo-tree-popup",
                     "notify"
                 },
                 buftype = {
-                    'terminal',
-                    "quickfix"
+                    -- 'terminal',
+                    -- "quickfix"
                 },
             },
         }
-    }
+    },
+    keys = {
+        {
+            "<leader>w",
+            mode = "n",
+            function()
+                local win = require("window-picker").pick_window({
+                })
+                if win then
+                    vim.api.nvim_set_current_win(win)
+                end
+            end,
+            desc = "Pick Window",
+        },
+    },
 }
