e66fe0fb6df1dd3e7f9764faba97d40b250ff480
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2026-03-03 22:16:21 +0800

    feat(nvim): modify treesitter-context style

--- config/nvim/lua/plugins/treesitter/treesitter_context.lua
+++ config/nvim/lua/plugins/treesitter/treesitter_context.lua
@@ -5,21 +5,21 @@ return {
     opts = {
         max_lines = 1,
         -- mode = 'topline',
-        separator = (function ()
-            if not vim.g.neovide then
-                return '─'
-            end
-        end)(),
+        -- separator = (function ()
+        --     if not vim.g.neovide then
+        --         return '─'
+        --     end
+        -- end)(),
     },
     config = function(_, opts)
         require("treesitter-context").setup(opts)
-        local brglng = require("brglng")
-        if not vim.g.neovide then
-            brglng.hl.transform_tbl {
-                TreesitterContext = { fg = "Normal.fg", bg = "Normal.bg" },
-                TreesitterContextSeparator = { fg = "WinSeparator.fg", bg = "Normal.bg" },
-                TreesitterContextLineNumber = { link = "LineNr" },
-            }
-        end
+        -- local brglng = require("brglng")
+        -- if not vim.g.neovide then
+        --     brglng.hl.transform_tbl {
+        --         TreesitterContext = { fg = "Normal.fg", bg = "Normal.bg" },
+        --         TreesitterContextSeparator = { fg = "WinSeparator.fg", bg = "Normal.bg" },
+        --         TreesitterContextLineNumber = { link = "LineNr" },
+        --     }
+        -- end
     end,
 }
