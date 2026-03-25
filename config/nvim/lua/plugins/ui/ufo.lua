7628108b93e1649df985173875f0a85a8a0020bf
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2026-03-25 20:21:56 +0800

    update

--- config/nvim/lua/plugins/ui/ufo.lua
+++ config/nvim/lua/plugins/ui/ufo.lua
@@ -10,20 +10,20 @@ return {
         vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
-    end,
-    config = function ()
-        require('ufo').setup({
-            provider_selector = function(bufnr, filetype, buftype)
-                return {'treesitter', 'indent'}
-            end,
-            disabled = { "neo-tree" }
-        })
-
-        vim.o.foldmethod = "expr"
-        vim.o.foldexpr = "nvim_treesitter#foldexpr()"
         vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+        -- vim.o.foldmethod = "expr"
+        -- vim.o.foldexpr = "nvim_treesitter#foldexpr()"
         -- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep:│,foldclose:]]
         -- vim.o.fillchars = [[eob: ,fold: ,foldopen:󰛲,foldsep:│,foldclose:󰜄]]
+    end,
+    opts = {
+        provider_selector = function(bufnr, filetype, buftype)
+            return { 'treesitter', 'indent' }
+        end,
+        disabled = { "neo-tree" }
+    },
+    config = function (_, opts)
+        require('ufo').setup(opts)
 
         vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
         vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
