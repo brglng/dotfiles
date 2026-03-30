7659f62a4846ee55db1f9800c525534fea48435b
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2026-03-30 13:41:22 +0800

    feat(nvim)!: switch treesitter to main branch

--- config/nvim/lua/plugins/treesitter/treesitter.lua
+++ config/nvim/lua/plugins/treesitter/treesitter.lua
@@ -1,19 +1,9 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufWritePost", "BufNewFile"  },
     lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    init = function(plugin)
-        -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-        -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-        -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-        -- Luckily, the only things that those plugins need are the custom queries, which we make available
-        -- during startup.
-        require("lazy.core.loader").add_to_rtp(plugin)
-        require("nvim-treesitter.query_predicates")
-    end,
     opts = {
         ensure_installed = {
             "bash",
@@ -81,34 +71,34 @@ return {
         }
     },
     config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
+        -- require("nvim-treesitter.configs").setup(opts)
 
-        -- require("nvim-treesitter").install(opts.ensure_installed)
-        -- if opts.auto_install then
-        --     vim.api.nvim_create_autocmd('FileType', {
-        --         group = vim.api.nvim_create_augroup('treesitter.setup', {}),
-        --         callback = function(args)
-        --             local buf = args.buf
-        --             local filetype = args.match
-        --
-        --             -- you need some mechanism to avoid running on buffers that do not
-        --             -- correspond to a language (like oil.nvim buffers), this implementation
-        --             -- checks if a parser exists for the current language
-        --             local language = vim.treesitter.language.get_lang(filetype) or filetype
-        --             if vim.treesitter.language.add(language) then
-        --                 vim.wo.foldmethod = 'expr'
-        --                 vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        --
-        --                 if opts.highlight.enable then
-        --                     vim.treesitter.start(buf, language)
-        --                 end
-        --
-        --                 if opts.indent.enable then
-        --                     vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        --                 end
-        --             end
-        --         end,
-        --     })
-        -- end
+        require("nvim-treesitter").install(opts.ensure_installed)
+        if opts.auto_install then
+            vim.api.nvim_create_autocmd('FileType', {
+                group = vim.api.nvim_create_augroup('treesitter.setup', {}),
+                callback = function(args)
+                    local buf = args.buf
+                    local filetype = args.match
+
+                    -- you need some mechanism to avoid running on buffers that do not
+                    -- correspond to a language (like oil.nvim buffers), this implementation
+                    -- checks if a parser exists for the current language
+                    local language = vim.treesitter.language.get_lang(filetype) or filetype
+                    if vim.treesitter.language.add(language) then
+                        vim.wo.foldmethod = 'expr'
+                        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+                        if opts.highlight.enable then
+                            vim.treesitter.start(buf, language)
+                        end
+
+                        if opts.indent.enable then
+                            vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                        end
+                    end
+                end,
+            })
+        end
     end
 }
