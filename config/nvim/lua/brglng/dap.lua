3b417c159c3c770bbf40a824406960c6870b3758
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2026-08-06 09:17:32 +0800

    chore(dap): disable project-local config loading

--- config/nvim/lua/brglng/dap.lua
+++ config/nvim/lua/brglng/dap.lua
@@ -2,21 +2,21 @@ local M = {}
 
 function M.start_debugging()
     local dap = require("dap")
-    local cwd = vim.fn.getcwd()
-    local paths = {
-        cwd .. "/.nvim/dap.lua",
-        cwd .. "/.vim/dap.lua",
-        cwd .. "/dap.lua",
-    }
-    local ok = false
-    local config = nil
-    for _, path in pairs(paths) do
-        ok, config = pcall(dofile, path)
-        if ok then
-            break
-        end
-    end
-    dap.configurations[vim.o.filetype] = config
+    -- local cwd = vim.fn.getcwd()
+    -- local paths = {
+    --     cwd .. "/.nvim/dap.lua",
+    --     cwd .. "/.vim/dap.lua",
+    --     cwd .. "/dap.lua",
+    -- }
+    -- local ok = false
+    -- local config = nil
+    -- for _, path in pairs(paths) do
+    --     ok, config = pcall(dofile, path)
+    --     if ok then
+    --         break
+    --     end
+    -- end
+    -- dap.configurations[vim.o.filetype] = config
     require("dapui").open()
     dap.continue()
 end
