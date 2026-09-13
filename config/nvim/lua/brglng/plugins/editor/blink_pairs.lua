afabb4a2d2ab0ae547f24e1d1a94f3880b41956c
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2026-09-13 21:34:59 +0800

    chore(nvim): use prebuilt blink pairs binary

--- config/nvim/lua/brglng/plugins/editor/blink_pairs.lua
+++ config/nvim/lua/brglng/plugins/editor/blink_pairs.lua
@@ -5,9 +5,10 @@ return {
 
     -- download prebuilt binaries from github releases, must be on a versioned release
     -- build = function() require('blink.pairs').download():pwait(60000) end,
+    build = function() require('blink.pairs').download():pwait(60000) end,
 
     -- OR build from source
-    build = function() require('blink.pairs').build():pwait(60000) end,
+    -- build = function() require('blink.pairs').build():pwait(60000) end,
 
     event = { 'InsertEnter', 'CmdlineEnter' },
     opts = {
